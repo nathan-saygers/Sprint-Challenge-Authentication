61e4414b585280c6a27683cdb854ce123c8f7d33
nathan-saygers <nathan.saygers@gmail.com>
2020-01-31 09:08:25 -0800

    add cross env and index route tests

---
+++ api/server.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+
+const server = require("./server.js");
+
+describe("server.js", () => {
+  describe("index route", () => {
+    it("should return an OK status from the index route", async () => {
+      const response = await request(server).get("/");
+      expect(response.status).toEqual(200);
+    });
+    it("should return a JSON object from index", async () => {
+      const response = await request(server).get("/");
+      expect(response.type).toEqual("application/json");
+    });
+  });
+});
