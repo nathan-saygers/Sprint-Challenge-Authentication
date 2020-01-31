1cc6f22fe3548b9bb30b9c5f064ffd169df44054
nathan-saygers <nathan.saygers@gmail.com>
2020-01-31 12:30:51 -0800

    added tests for jokes router

--- jokes/jokes-router.test.js
+++ jokes/jokes-router.test.js
@@ -1,16 +1,25 @@
 const request = require("supertest");
 
-const jokesRouter = require("./jokes-router.js");
+// const jokesRouter = require("./jokes-router.js");
+
+const server = require("../api/server.js");
 
 describe("jokes-router.js", function() {
   it("runs the tests", function() {
     return expect(true).toBe(true);
   });
 
-  // describe("index route", () => {
-  //   it("should return an OK status from the jokes", async () => {
-  //     const response = await request(jokesRouter).get("/");
-  //     expect(response.status).toEqual(200);
-  //   });
-  // });
+  describe("joke route auth", () => {
+    it("Jokes route returns 401 without auth token", async () => {
+      const response = await request(server).get("/api/jokes");
+      expect(response.status).toEqual(401);
+    });
+  });
+  it("should return JSON", function() {
+    return request(server)
+      .get("/api/jokes")
+      .then(res => {
+        expect(res.type).toMatch(/json/i);
+      });
+  });
 });
